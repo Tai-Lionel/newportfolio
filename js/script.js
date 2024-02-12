2d3280bf5dcd5f21bddd3b6812ba779da1c55578
Tai-Lionel <tailionel240687@gmail.com>
2024-02-12 20:20:13 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// take the text from the typing-name
+const heading = document.querySelector("#typing-name");
+const text = "I am Tai Lionel";
+for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+        heading.textContent += text[i];
+    }, 100 * (i + 1));
+}
